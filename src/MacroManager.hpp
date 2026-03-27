cd4ac5c807ce70b8dc96bab1b23853f4081924d9
YallaY <yallcc.info@gmail.com>
2026-03-27 23:42:13 +0100

    Update MacroManager.hpp

--- src/MacroManager.hpp
+++ src/MacroManager.hpp
@@ -5,12 +5,11 @@
 
 using namespace geode::prelude;
 
-// A single recorded input event
 struct MacroInput {
-    int frame;        // Physics frame number when this input happened
-    int button;       // 1 = jump (left), 2 = jump (right)
-    bool player2;     // false = player 1, true = player 2
-    bool down;        // true = press, false = release
+    int frame;
+    int button;
+    bool player2;
+    bool down;
 };
 
 enum class BotState {
@@ -23,29 +22,25 @@ class MacroManager {
 public:
     static MacroManager& get();
 
-    // State
     BotState state = BotState::Idle;
     std::vector<MacroInput> inputs;
     int currentFrame = 0;
     int playbackIndex = 0;
+    bool ignoreInputs = true;
 
-    // Actions
     void startRecording();
     void stopRecording();
     void startPlayback();
     void stopPlayback();
     void clearMacro();
 
-    // Called every physics frame during recording/playback
     void onFrameAdvance(GJBaseGameLayer* layer);
-
-    // Called when player presses/releases a button
     void onInput(int button, bool player2, bool down);
 
-    // Save/load
-    bool saveMacro(const std::string& filename);
-    bool loadMacro(const std::string& filename);
-
+    bool saveMacro(const std::string& name);
+    bool loadMacro(const std::string& name);
+    bool deleteMacro(const std::string& name);
+    std::vector<std::string> getReplayList();
     std::string getSaveDir();
 
 private:
